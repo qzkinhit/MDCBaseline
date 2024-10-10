f9c738661b6ae4a59d56ed2b4a2e523d56cede39
qzkinhit <qzk010728@gmail.com>
2024-10-10 19:42:17 +0800

    规范化脚本文件和指标文件,优化和修正getscore

--- util/insert_null.py
+++ util/insert_null.py
@@ -26,16 +26,18 @@ def inject_missing_values(csv_file, output_file, attributes_error_ratio, missing
     df.replace('null', missing_value_representation, inplace=True)
     df.replace('__NULL__', missing_value_representation, inplace=True)
     df.replace(missing_value_in_ori_data, missing_value_representation, inplace=True)
-
+    if attributes_error_ratio is None:
+        print("没有指定错误比例，仅进行原数据集的空值替换，不添加错误")
+    else:
     # 遍历每个属性，注入空值
-    for attribute, error_ratio in attributes_error_ratio.items():
-        if attribute in df.columns:
-            num_rows = len(df)
-            num_errors = int(num_rows * error_ratio / 100)
-            error_indices = random.sample(range(num_rows), num_errors)
+        for attribute, error_ratio in attributes_error_ratio.items():
+            if attribute in df.columns:
+                num_rows = len(df)
+                num_errors = int(num_rows * error_ratio / 100)
+                error_indices = random.sample(range(num_rows), num_errors)
 
-            # 替换指定行的值为空值表达方式
-            df.loc[error_indices, attribute] = missing_value_representation
+                # 替换指定行的值为空值表达方式
+                df.loc[error_indices, attribute] = missing_value_representation
 
     # 保存注入错误后的CSV文件
     df.to_csv(output_file, index=False)
@@ -59,6 +61,14 @@ inject_missing_values(
     csv_file='../Data/5_tax/tax_200k/dirty_mix_2/dirty_tax.csv',
     output_file='../Data/5_tax/tax_200k/dirty_mix_2/dirty_tax_null.csv',
     attributes_error_ratio=attributes_error_ratio,
+    missing_value_in_ori_data='NULL',
+    missing_value_representation='empty'
+)
+# 如果干净数据存在空值，记得替换clean数据中的空值，统一转换为empty
+inject_missing_values(
+    csv_file='../Data/5_tax/clean.csv',
+    output_file='../Data/5_tax/clean.csv',
+    attributes_error_ratio=None,
     missing_value_in_ori_data='NULL',
     missing_value_representation='empty'
 )
