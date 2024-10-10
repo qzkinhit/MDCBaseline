5cccdfb76fcfab730b1364279c5b9277620a3096
DELL <qzk010728@gmail.com>
2024-10-10 13:44:36 +0800

    添加对比程序

---
+++ util/get_error_num.py
@@ -0,0 +1,98 @@
+# import pandas as pd
+#
+# df1 = pd.read_csv('dirty_hospitals.csv')
+# df2 = pd.read_csv('hospital_clean_index.csv')
+#
+# # 将所有列转换为字符串
+# for col in df1.columns:
+#     df1[col] = df1[col].astype(str)
+#
+# for col in df2.columns:
+#     df2[col] = df2[col].astype(str)
+#
+# # 计算不同的条目数
+# num_different_rows = 0
+# for index, row1 in df1.iterrows():
+#     row2 = df2.loc[index]
+#     if not row1.equals(row2):
+#         num_different_rows += 1
+# total_rows = df1.shape[0]
+# print(f"错误条目数：{num_different_rows}")
+# print(f"错误条目比例：{num_different_rows/total_rows}")
+
+import pandas as pd
+
+def count_inconsistent_entries(dirty_df, clean_df, index_column):
+    """
+    计算脏数据和干净数据中不一致的条目数量
+
+    :param dirty_df: 脏数据 DataFrame
+    :param clean_df: 干净数据 DataFrame
+    :param index_column: 用于对齐的索引列名称
+    :return: 不一致条目数
+    """
+    # 确保脏数据和干净数据以相同的索引进行对齐，并将所有列转换为字符串类型
+    dirty_df = dirty_df.set_index(index_column).astype(str)
+    clean_df = clean_df.set_index(index_column).astype(str)
+
+    # 初始化不一致条目的集合
+    inconsistent_entry_indices = set()
+
+    # 遍历所有列，查找脏数据和干净数据之间的不一致单元格
+    for column in dirty_df.columns:
+        # 查找在当前列中脏数据和干净数据值不一致的单元格
+        mismatched_indices = dirty_df.index[(dirty_df[column] != clean_df[column])]
+
+        # 将不一致的索引添加到集合中
+        inconsistent_entry_indices.update(mismatched_indices)
+
+    # 返回不一致条目的数量
+    return len(inconsistent_entry_indices)
+
+
+def generate_change_report(dirty_df, clean_df, index_column):
+    """
+    比较脏数据和干净数据的单元格变化情况，生成change.CSV文件
+
+    :param dirty_df: 脏数据 DataFrame
+    :param clean_df: 干净数据 DataFrame
+    :param index_column: 用于对齐的索引列名称
+    :return: 不一致单元格数目，并生成 change.CSV 文件
+    """
+    # 确保脏数据和干净数据以相同的索引进行对齐，并将所有列转换为字符串类型
+    dirty_df = dirty_df.set_index(index_column).astype(str)
+    clean_df = clean_df.set_index(index_column).astype(str)
+    # 初始化列表，用于存储变化信息
+    changes = []
+
+    # 遍历所有列，查找脏数据和干净数据之间的不一致单元格
+    for column in dirty_df.columns:
+        # 查找在当前列中脏数据和干净数据值不一致的单元格
+        mismatched_indices = dirty_df.index[(dirty_df[column] != clean_df[column])]
+
+        for idx in mismatched_indices:
+            changes.append({
+                'index': idx,
+                'attribute': column,
+                'dirty_value': dirty_df.at[idx, column],
+                'clean_value': clean_df.at[idx, column]
+            })
+
+    # 将变化信息存储到DataFrame中
+    change_df = pd.DataFrame(changes)
+
+    # 将结果保存为CSV文件
+    change_df.to_csv('change.CSV', index=False)
+    print("不同单元的数据保存到 change.CSV")
+    # 返回不一致的单元格总数
+    return len(change_df)
+
+# 使用示例
+dirty_df = pd.read_csv('../Data/1_hospital/noise_with_correct_primary_key/dirty_mix_0.5/dirty_hospitals_index.csv')
+clean_df = pd.read_csv('../Data/1_hospital/hosptial_clean_index.csv')
+
+inconsistent_entries_count = count_inconsistent_entries(dirty_df, clean_df, 'index')
+print(f'脏数据和干净数据之间有 {inconsistent_entries_count} 个条目不一致。')
+
+inconsistent_cells = generate_change_report(dirty_df, clean_df, 'index')
+print(f'脏数据和干净数据之间有 {inconsistent_cells} 个单元格不一致。')
