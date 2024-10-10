56f3b77767eafbc09f4849acdccda70163333d56
Sososoby <799005719@qq.com>
2024-10-10 15:52:55 +0800

    add tax process program

---
+++ Data/5_tax/preparation.py
@@ -0,0 +1,25 @@
+import pandas as pd
+
+
+def indexData(input_path, output_path):
+    df = pd.read_csv(input_path)
+    df.insert(0, 'tno', range(len(df)))
+    df.to_csv(output_path, index=False)
+
+
+def separateCellChange(input_path, output_path):
+    df = pd.read_csv(input_path, header=None)
+    df[['key', 'value']] = df[0].str.split('.', expand=True)
+    df.to_csv(output_path, index=False, header=False)
+
+
+def countTupleDifference(clean_path, dirty_path):
+    df_dirty = pd.read_csv(dirty_path, dtype=str)
+    df_clean = pd.read_csv(clean_path, dtype=str)
+    differences = df_dirty.ne(df_clean).any(axis=1)
+    print(differences.sum())
+
+
+# indexData("tax_200k/tax_200k_clean.csv", "tax_200k/tax_200k_clean_id.csv")
+# separateCellChange("tax_200k/dirty_mix_0.5/cellChanges.csv", "tax_200k/dirty_mix_0.5/cell_changes.csv")
+countTupleDifference("tax_200k/tax_200k_clean_id.csv", "tax_200k/dirty_mix_0.5/dirty_tax.csv")
