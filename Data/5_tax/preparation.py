8c15d59e5bb2a1da95933bb93912ff68626bb178
Sososoby <799005719@qq.com>
2024-10-15 22:36:18 +0800

    完成tax数据集的所有错误注入

--- Data/5_tax/preparation.py
+++ Data/5_tax/preparation.py
@@ -29,7 +29,7 @@ def countTupleDifference(clean_path, dirty_path):
 
 # indexData("tax_20k/tax_20k_clean.csv", "tax_20k/tax_20k_clean_id.csv")
 # separateCellChange("tax_200k/dirty_mix_0.5/cellChanges.csv", "tax_200k/dirty_mix_0.5/cell_changes.csv")
-countCellDifference("tax_50k/tax_50k_clean_id.csv",
-                    "tax_50k/noise/dirty_mix_2/dirty_tax_null.csv")
-countTupleDifference("tax_50k/tax_50k_clean_id.csv",
-                     "tax_50k/noise/dirty_mix_2/dirty_tax_null.csv")
+countCellDifference("tax_200k/tax_200k_clean_id.csv",
+                    "tax_200k/noise/dirty_mix_0.25/dirty_tax_null.csv")
+countTupleDifference("tax_200k/tax_200k_clean_id.csv",
+                     "tax_200k/noise/dirty_mix_0.25/dirty_tax_null.csv")
