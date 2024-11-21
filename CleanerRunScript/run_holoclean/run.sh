6d58b11a8e06675e566f57d16aa9db47a84447ed
qzk <qzk010728@gmail.com>
2024-11-21 17:07:16 +0800

    fix run nwcpk sh

--- CleanerRunScript/run_holoclean/run.sh
+++ CleanerRunScript/run_holoclean/run.sh
@@ -4,20 +4,20 @@
 
 # 定义holoclean命令列表，包含每个数据集的mse_attributes，注意我这里在服务器上运行，所以是python3开头，你们本地可能只能识别python
 holoclean_commands=(
-    "python3 CleanerRunScript/run_holoclean/run_holoclean_base.py --clean_path Data/1_hospitals/clean_index.csv --dirty_path Data/1_hospitals/dirty_index.csv --rule_path Data/1_hospitals/dc_rules_dc_holoclean.txt --task_name 1_hospital_ori --output_path results/holoclean --mse_attributes Score"
-    "python3 CleanerRunScript/run_holoclean/run_holoclean_base.py --clean_path Data/2_flights/clean_index.csv --dirty_path Data/2_flights/dirty_index.csv --rule_path Data/2_flights/dc_rules_holoclean.txt --task_name 2_flights_ori --output_path results/holoclean"
-    "python3 CleanerRunScript/run_holoclean/run_holoclean_base.py --clean_path Data/3_beers/clean_index.csv --dirty_path Data/3_beers/dirty_index.csv --rule_path Data/3_beers/dc_rules_holoclean.txt --task_name 3_beers_ori --index_attribute id --output_path results/holoclean --mse_attributes abv ibu"
-    "python3 CleanerRunScript/run_holoclean/run_holoclean_base.py --clean_path Data/4_rayyan/clean_index.csv --dirty_path Data/4_rayyan/dirty_index.csv --rule_path Data/4_rayyan/dc_rules_holoclean.txt --task_name 4_rayyan_ori --output_path results/holoclean"
+#    "python3 CleanerRunScript/run_holoclean/run_holoclean_base.py --clean_path Data/1_hospitals/clean_index.csv --dirty_path Data/1_hospitals/dirty_index.csv --rule_path Data/1_hospitals/dc_rules_dc_holoclean.txt --task_name 1_hospital_ori --output_path results/holoclean --mse_attributes Score"
+#    "python3 CleanerRunScript/run_holoclean/run_holoclean_base.py --clean_path Data/2_flights/clean_index.csv --dirty_path Data/2_flights/dirty_index.csv --rule_path Data/2_flights/dc_rules_holoclean.txt --task_name 2_flights_ori --output_path results/holoclean"
+#    "python3 CleanerRunScript/run_holoclean/run_holoclean_base.py --clean_path Data/3_beers/clean_index.csv --dirty_path Data/3_beers/dirty_index.csv --rule_path Data/3_beers/dc_rules_holoclean.txt --task_name 3_beers_ori --index_attribute id --output_path results/holoclean --mse_attributes abv ibu"
+#    "python3 CleanerRunScript/run_holoclean/run_holoclean_base.py --clean_path Data/4_rayyan/clean_index.csv --dirty_path Data/4_rayyan/dirty_index.csv --rule_path Data/4_rayyan/dc_rules_holoclean.txt --task_name 4_rayyan_ori --output_path results/holoclean"
     "python3 CleanerRunScript/run_holoclean/run_holoclean_base.py --clean_path Data/5_tax/clean_index_10k.csv --dirty_path Data/5_tax/dirty_index_10k.csv --rule_path Data/5_tax/dc_rules_holoclean.txt --task_name 5_tax_ori --output_path results/holoclean --mse_attributes rate"
     "python3 CleanerRunScript/run_holoclean/run_holoclean_base.py --clean_path Data/6_soccer/clean_index_10k.csv --dirty_path Data/6_soccer/dirty_index_10k.csv --rule_path Data/6_soccer/dc_rules_holoclean.txt --task_name 6_soccer_ori --output_path results/holoclean"
 )
 
 # 定义日志文件名列表
 log_files=(
-    "logs/1_hospital_ori_holoclean.log"
-    "logs/2_flights_ori_holoclean.log"
-    "logs/3_beers_ori_holoclean.log"
-    "logs/4_rayyan_ori_holoclean.log"
+#    "logs/1_hospital_ori_holoclean.log"
+#    "logs/2_flights_ori_holoclean.log"
+#    "logs/3_beers_ori_holoclean.log"
+#    "logs/4_rayyan_ori_holoclean.log"
     "logs/5_tax_10k_ori_holoclean.log"
     "logs/6_soccer_10k_ori_holoclean.log"
 )
--- CleanerRunScript/run_nwcpk_example.sh
+++ CleanerRunScript/run_nwcpk_example.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
-# 设置可执行权限： chmod +x CleanerRunScript/run_raha_baran/run_nwcpk_example.sh
-# 运行方式：./CleanerRunScript/run_raha_baran/run_nwcpk_example.sh
+# 设置可执行权限： chmod +x CleanerRunScript/run_raha_baran/run_nwcpk.sh
+# 运行方式：./CleanerRunScript/run_raha_baran/run_nwcpk.sh
 
 # 定义数据集配置
 # 解析dataset index_attr mse_attr noise_dir clean_path这几个参数，后续需要，根据你的系统配置自行修改
