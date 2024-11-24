5ffeb22d51e80dd187f1c32265fda34d3c9bde39
lzfd <1953802638@qq.com>
2024-11-24 19:43:32 +0800

    更新baran上部分结果

--- CleanerRunScript/run_raha_baran/run_nwcpk.sh
+++ CleanerRunScript/run_raha_baran/run_nwcpk.sh
@@ -7,17 +7,14 @@
 datasets=(
     "1_hospitals:index:Score:Data/1_hospitals/noise_with_correct_primary_key:Data/1_hospitals/clean_index.csv"
     "2_flights:index::Data/2_flights/noise_with_correct_primary_key:Data/2_flights/clean_index.csv"
-    "3_beers:id:abv ibu:Data/3_beers/noise_with_correct_primary_key:Data/3_beers/clean.csv"
+    "3_beers:id:abv ibu:Data/3_beers/noise_with_correct_primary_key:Data/3_beers/clean_index.csv"
     "4_rayyan:index::Data/4_rayyan/noise_with_correct_primary_key:Data/4_rayyan/clean_index.csv"
-    "5_tax_20k:tno:rate:Data/5_tax/tax_20k/noise_with_correct_primary_key:Data/5_tax/tax_20k/tax_20k_clean_id.csv"
-    "5_tax_50k:tno:rate:Data/5_tax/tax_50k/noise_with_correct_primary_key:Data/5_tax/tax_50k/tax_50k_clean_id.csv"
-    "5_tax_200k:tno:rate:Data/5_tax/tax_200k/noise_with_correct_primary_key:Data/5_tax/tax_200k/tax_200k_clean_id.csv"
-    "6_soccer:index::Data/6_soccer/noise_with_correct_primary_key:Data/6_soccer/clean_index.csv"
+    "5_tax:tno:rate:Data/5_tax/subset_tax_10k/noise_with_correct_primary_key:Data/5_tax/subset_tax_10k/subset_tax_10k_clean_index.csv"
+    "6_soccer:index::Data/6_soccer/subset_directly_extract_soccer_10k/noise_with_correct_primary_key:Data/6_soccer/subset_directly_extract_soccer_10k/subset_directly_extract_soccer_10k_clean_index.csv"
 )
 
 # 定义错误比例集合
-# error_ratios=("0.25" "0.5" "0.75" "1" "1.25" "1.5" "1.75" "2")
-error_ratios=("1.25" "1.5" "1.75" "2")
+error_ratios=("0.25" "0.5" "0.75" "1" "1.25" "1.5" "1.75" "2")
 
 # 创建日志目录
 log_dir="logs/raha_baran_nwcpk"
